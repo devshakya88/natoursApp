af89cd01db0fae4aed5b9eaa7aca7533cf7a10c8
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-07 13:35:11 +0530

    Pagination Done

--- 4-natours/controllers/tourController.js
+++ 4-natours/controllers/tourController.js
@@ -4,6 +4,7 @@ const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
+
     // 1(a). Filtering
     const queryObj = { ...req.query };
     console.log(req.query);
@@ -25,6 +26,27 @@ exports.getAllTours = async (req, res) => {
       query = query.sort(req.query.sort);
     } else {
       query = query.sort('-createdAt');
+    }
+
+    //3. Field Limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
+    //4. Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) {
+        throw new Error('This page doest not exit');
+      }
     }
 
     // Execute Query
