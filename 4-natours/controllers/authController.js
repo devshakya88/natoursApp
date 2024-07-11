7c4f94b41209266bc53836f766aaea8a54c14c5f
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-12 00:40:31 +0530

    A

--- 4-natours/controllers/authController.js
+++ 4-natours/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
@@ -31,21 +32,50 @@ exports.signup = catchAsync(async (req, res, next) => {
 exports.login = catchAsync(async (req, res, next) => {
   const { email, password } = req.body;
 
-  //1.  Check  if email and Password exist
+  // 1. Check if email and password exist
   if (!email || !password) {
-    next(new AppError('Please Provide email and password', 400));
+    return next(new AppError('Please provide email and password!', 400));
   }
 
-  //2. Check if user exists && password is correct
+  // 2. Check if user exists && password is correct
   const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new AppError('Incorrect email or password', 401));
   }
-  //3. if everthing ok, sned token to client
+
+  // 3. If everything is ok, send token to client
   const token = signToken(user._id);
   res.status(200).json({
     status: 'success',
     token,
   });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
+  // 1. Getting token and check if it's there
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401),
+    );
+  }
+
+  // 2. Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
+  console.log(decoded);
+  // 3. Check if user still exists
+
+  // 4. Check if user changed password after the token was issued
+
+  // Grant access to protected route
+
+  next();
 });
