4b4059fd778c19a0341ada554d73f8eb4c8c7900
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-19 10:04:00 +0530

    Adding Mission:Authentication & Authorization

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -32,7 +32,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 //Body Pareser, readinf data from body into req.body
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: '100kb' }));
 
 //Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
