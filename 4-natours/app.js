07e2be1b981eb93e22b9f6f37072dbb25f9814f6
Devender Shakya <135866940+devshakya88@users.noreply.github.com>
2024-07-21 19:08:27 +0530

    Creating Your Base Template

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -54,8 +54,12 @@ app.use((req, res, next) => {
 ///New Comment
 // Routes
 app.get('/', (req, res) => {
-  res.status(200).render('base');
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
 });
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
